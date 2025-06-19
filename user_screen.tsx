40c1efb5b12f44599769d6cbc314d9340d56ee31
singhalaryann <aryan.singhal.20cse@bmu.edu.in>
2025-06-19 15:42:19 +0530

    user_screen page added

---
+++ user_screen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function user_list() {
+  return (
+    <View>
+      <Text>user_list</Text>
+    </View>
+  )
+}
